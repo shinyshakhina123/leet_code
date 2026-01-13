7ff247ad3d301a4f1a962ba2ce880232a324777c
shiny <shinyshakhina08@gmail.com>
2026-01-14 00:18:25 +0530

    Implement separateSquares function for area calculation

--- separate _squares.c
+++ separate _squares.c
@@ -1,1 +1,50 @@
-https://leetcode.com/problems/separate-squares-i/
+double separateSquares(int** squares, int squaresSize, int* squaresColSize) {
+
+    double totalArea = 0.0;
+    double low = 1e18, high = 0.0;
+
+    // Calculate total area and y-range
+    for (int i = 0; i < squaresSize; i++) {
+        double y = squares[i][1];
+        double l = squares[i][2];
+
+        totalArea += l * l;
+
+        if (y < low) low = y;
+        if (y + l > high) high = y + l;
+    }
+
+    double target = totalArea / 2.0;
+
+    // Binary search
+    for (int iter = 0; iter < 60; iter++) {
+
+        double mid = (low + high) / 2.0;
+        double areaBelow = 0.0;
+
+        // Compute area below mid
+        for (int i = 0; i < squaresSize; i++) {
+            double y = squares[i][1];
+            double l = squares[i][2];
+
+            if (mid <= y) {
+                continue;
+            }
+            else if (mid >= y + l) {
+                areaBelow += l * l;
+            }
+            else {
+                areaBelow += l * (mid - y);
+            }
+        }
+
+        if (areaBelow < target) {
+            low = mid;
+        } else {
+            high = mid;
+        }
+    }
+
+    return low;
+}
+
